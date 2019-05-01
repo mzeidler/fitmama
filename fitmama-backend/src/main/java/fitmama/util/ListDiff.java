b1a0961b56740e78ec30cf1fa4f4ff9bfa629b4e
mzeidler <mzeidler3@gmail.com>
2019-05-01 13:47:58 +0200

    /api/menus/test removed

---
+++ fitmama-backend/src/main/java/fitmama/util/ListDiff.java
@@ -0,0 +1,33 @@
+package fitmama.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDiff<T> {
+
+	private List<T> leftList;
+	private List<T> rightList;
+
+	public ListDiff(List<T> leftList, List<T> rightList) {
+		this.leftList = new ArrayList<>(leftList);
+		this.rightList = new ArrayList<>(rightList);
+	}
+
+	public List<T> leftOnly() {
+		List<T> tmp = new ArrayList<>(leftList);
+		tmp.removeAll(rightList);
+		return tmp;
+	}
+
+	public List<T> rightOnly() {
+		List<T> tmp = new ArrayList<>(rightList);
+		tmp.removeAll(leftList);
+		return tmp;
+	}
+
+	public List<T> inCommon() {
+		List<T> tmp = new ArrayList<>(rightList);
+		tmp.retainAll(leftList);
+		return tmp;
+	}
+}
