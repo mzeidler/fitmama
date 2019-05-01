97430752f0a6a88e9fa01ae4fcc34b296e948292
mzeidler <mzeidler3@gmail.com>
2019-05-01 13:59:21 +0200

    Add/Remove Menu/Role/Training from/to User (Backend)

--- fitmama-backend/src/main/java/fitmama/service/UserService.java
+++ fitmama-backend/src/main/java/fitmama/service/UserService.java
@@ -6,6 +6,7 @@ import fitmama.model.Training;
 import fitmama.repo.MenuRepository;
 import fitmama.repo.RoleRepository;
 import fitmama.repo.TrainingRepository;
+import fitmama.util.ListDiff;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -52,53 +53,63 @@ public class UserService {
 
     public User updateTrainings(User user) {
         User userDB = getUser(user.getId());
-        if (userDB != null) {
-            user.getTrainings().forEach(training -> {
-                if (!userDB.getTrainings().contains(training)) {
-                    Training trainingDB = getTraining(training.getId());
-                    if (trainingDB != null) {
-                        userDB.getTrainings().add(trainingDB);
-                    }
-                }
-            });
-            userDB.getTrainings().retainAll(user.getTrainings());
-            return userRepository.saveAndFlush(userDB);
-        }
-        return null;
+
+        ListDiff<Training> diff = new ListDiff(user.getTrainings(), userDB.getTrainings());
+
+        diff.leftOnly().forEach(trainingToAdd -> {
+            Training trainingDB = getTraining(trainingToAdd.getId());
+            trainingDB.getUsers().add(userDB);
+            trainingRepository.saveAndFlush(trainingDB);
+        });
+
+        diff.rightOnly().forEach(trainingToRemove -> {
+            Training trainingDB = getTraining(trainingToRemove.getId());
+            trainingDB.getUsers().remove(userDB);
+            trainingRepository.saveAndFlush(trainingDB);
+        });
+
+        return userDB;
     }
 
     public User updateMenus(User user) {
         User userDB = getUser(user.getId());
-        if (userDB != null) {
-            user.getMenus().forEach(menu -> {
-                if (!userDB.getMenus().contains(menu)) {
-                    Menu menuDB = getMenu(menu.getId());
-                    if (menuDB != null) {
-                        userDB.getMenus().add(menuDB);
-                    }
-                }
-            });
-            userDB.getMenus().retainAll(user.getMenus());
-            return userRepository.saveAndFlush(userDB);
-        }
-        return null;
+
+        ListDiff<Menu> diff = new ListDiff(user.getMenus(), userDB.getMenus());
+
+        diff.leftOnly().forEach(menuToAdd -> {
+            Menu menuDB = getMenu(menuToAdd.getId());
+            menuDB.getUsers().add(userDB);
+            menuRepository.saveAndFlush(menuDB);
+        });
+
+        diff.rightOnly().forEach(menuToRemove -> {
+            Menu menuDB = getMenu(menuToRemove.getId());
+            menuDB.getUsers().remove(userDB);
+            menuRepository.saveAndFlush(menuDB);
+        });
+
+        return userDB;
     }
 
     public User updateRoles(User user) {
         User userDB = getUser(user.getId());
-        if (userDB != null) {
-            user.getRoles().forEach(role -> {
-                if (!userDB.getRoles().contains(role)) {
-                    Role roleDB = getRole(role.getId());
-                    userDB.getRoles().add(roleDB);
-                }
-            });
-            userDB.getRoles().retainAll(user.getRoles());
-            return userRepository.saveAndFlush(userDB);
-        }
-        return null;
-    }
 
+        ListDiff<Role> diff = new ListDiff(user.getRoles(), userDB.getRoles());
+
+        diff.leftOnly().forEach(roleToAdd -> {
+            Role roleDB = getRole(roleToAdd.getId());
+            roleDB.getUsers().add(userDB);
+            roleRepository.saveAndFlush(roleDB);
+        });
+
+        diff.rightOnly().forEach(roleToRemove -> {
+            Role roleDB = getRole(roleToRemove.getId());
+            roleDB.getUsers().remove(userDB);
+            roleRepository.saveAndFlush(roleDB);
+        });
+
+        return userDB;
+    }
     // Helper methods
 
     public User getUser(Long id) {
